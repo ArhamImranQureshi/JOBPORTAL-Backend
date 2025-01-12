18b0f01868e2253bd032eab294da88db34b77afa
Arham Qureshi <arhamgameslover@gmail.com>
2025-01-12 05:33:25 +0500

    .

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,8 @@
 import {User} from '../models/user.models.js';
 import bycrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import getDataUri from '../utils/datauri.js';
+import cloudinary from '../utils/cloudinary.js';
 
 export const register = async(req,res)=>{
     try{
@@ -100,12 +102,23 @@ export const updateProfile = async(req,res)=>{
     try{
         const {fullname, email, phoneNumber, bio, skills} = req.body;
         const file = req.file;
+        ///// cloudinary is implimented
+        const fileUri = getDataUri(file);
+        const cloudResponce = await cloudinary.uploader.upload(fileUri.content);
+
+
+
+
         if(!fullname || !email || !phoneNumber || !skills || !bio ){
             return res.status(400).json({
                 message:"Something is missing",
                 success:false,
             });
         };
+
+
+
+
 
         const skillArray = skills? skills.split(","):[];
         const userId = req.id;
@@ -123,6 +136,12 @@ export const updateProfile = async(req,res)=>{
         if(bio) user.profile.bio = bio;
         if(skills) user.profile.skills =skillArray;
         
+        // this will save the cloudairy url to the user profile
+        if(cloudResponce){
+            user.profile.resume = cloudResponce.secure_url;
+            user.profile.resumeOriginalName = file.originalname /// to show the originname of the file
+
+        }
         await user.save();
         user = {
             _id: user._id,
